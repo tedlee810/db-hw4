791186481e0e84273f4c47df074a9dacc7ace703
Ted Lee <tedlee@gala.local>
2022-04-21 01:22:15 -0400

    fixed CurPasswords typo

--- HW4_ShowAllRawScores.sql
+++ HW4_ShowAllRawScores.sql
@@ -9,7 +9,7 @@ CREATE PROCEDURE ShowAllRawScores(IN pass VARCHAR(15))
 BEGIN
     -- TODO: fill this section in
     SELECT S.SID, S.LName, S.FName, S.Sec, R.Score
-    FROM HW4_Student AS S LEFT OUTER JOIN HW4_RawScore AS R, HW4_Password AS P
+    FROM HW4_Password AS P, HW4_Student AS S LEFT OUTER JOIN HW4_RawScore AS R
     ON S.SID = R.SID
     WHERE P.CurPasswords = pass
     ORDER BY S.Sec ASC, S.LName ASC, S.FName ASC;
