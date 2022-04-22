76258bd202650d592fb51190a69990eda385b16e
Ted Lee <tedlee@gala.acasmart.jh.edu>
2022-04-22 16:39:13 -0400

    messing with ShowPercentages.sql

--- HW4_ShowPercentages.sql
+++ HW4_ShowPercentages.sql
@@ -34,7 +34,7 @@ BEGIN
         ON R.AName = A.AName)
     SELECT S.SID, S.LName, S.FName, S.Sec, R.AName, P.Percent--, ROUND(( (SUM(Quizzes.Score) / COUNT(Quizzes.Score)) * 0.4 + (SUM(Exams.Score) / COUNT(Exams.Score)) * 0.6 ), 2) AS CourseAvg
     FROM HW4_Student AS S, HW4_RawScore AS R, Percentages AS P, Quizzes, Exams
-    WHERE S.SID = R.SID AND Quizzes.SID = S.SID AND Exams.SID = S.SID;
+    WHERE S.SID = R.SID --AND Quizzes.SID = S.SID AND Exams.SID = S.SID;
 
 END $
 DELIMITER ;
